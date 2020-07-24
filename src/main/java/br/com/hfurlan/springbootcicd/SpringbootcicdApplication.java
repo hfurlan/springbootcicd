0949995f4be5649afd3ac7774dafb70d93fc6a54
hfurlan <hildebrando.furlan@gmail.com>
2020-07-24 06:26:24 -0300

    Novos ajustes v1

---
+++ src/main/java/br/com/hfurlan/springbootcicd/SpringbootcicdApplication.java
@@ -0,0 +1,13 @@
+package br.com.hfurlan.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcicdApplication.class, args);
+	}
+
+}
